b2842b5d00597874b65cf5d5e3db9fbabff7e81b
Muhless <nuryadi.muhta@gmail.com>
2025-09-29 10:01:39 +0700

    fix: customer model

--- internal/model/customer.go
+++ internal/model/customer.go
@@ -1,8 +1,8 @@
 package model
 
 type Customer struct {
-	ID   uint   `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name string `json:"name"`
-	Phone 
+	ID      uint   `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name    string `json:"name"`
+	Phone   string `json:"phone"`
 	Address string `json:"address"`
 }
