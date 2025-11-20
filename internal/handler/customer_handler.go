b18c5627d184d341fbf6c660dd55c938f7c613b7
Muhless <nuryadi.muhta@gmail.com>
2025-11-20 10:28:11 +0700

    add create customer controller

--- internal/handler/customer_handler.go
+++ internal/handler/customer_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 	"pd_pritani/internal/config"
 	"pd_pritani/internal/model/customer"
@@ -34,5 +35,31 @@ func GetCustomer(ctx *gin.Context) {
 }
 
 func CreateCustomer(ctx *gin.Context) {
+	var customer customer.Customer
+
+	if err := ctx.ShouldBindJSON(&customer); err != nil {
+		log.Println("JSON bind error:", err)
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"message": "Invalid JSON format",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	if err := config.DB.Create(&customer).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"message": "Failed to create customer data",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"success":  true,
+		"message":  "Successfully created customer data",
+		"customer": customer,
+	})
 
 }
